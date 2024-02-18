879772e8ee8fc5900e1de98e432bb4f314067a31
Tirth patel <tirthdhvani@gmail.com>
2024-02-18 18:20:50 +0530

    we connect buttons to files to store our todos.

--- gui.py
+++ gui.py
@@ -2,11 +2,26 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("ADD")
 
-window = sg.Window('My to-do App', layout=[[label], [input_box, add_button]])
-window.read()
-print("Window successfully closed")
+window = sg.Window('My to-do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "ADD":
+            todos = functions.get_todos()
+            new_todo = value['todo'] + '\n'
+            todos.append(new_todo)
+            functions.set_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
 
