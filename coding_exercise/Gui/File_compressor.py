a9997f7b238c9bf51f39dc9e30b543e02474922a
Tirth patel <tirthdhvani@gmail.com>
2024-02-18 10:59:02 +0530

    We create basic structure for File compressors.

---
+++ coding_exercise/Gui/File_compressor.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+
+label1 = sg.Text("Selects to compress file")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose")
+
+label2 = sg.Text("Selects destination folder")
+input2 = sg.Input()
+choose_button2 = sg.FilesBrowse("Choose")
+
+compress_button = sg.Button("Compress")
+
+window = sg.Window("File compressor", layout=[[label1, input1, choose_button1],
+                                              [label2, input2, choose_button2],
+                                              [compress_button]])
+window.read()
+windew.close()
